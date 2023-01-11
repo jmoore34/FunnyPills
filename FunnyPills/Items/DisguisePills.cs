368d8ceb0f6597a2938f4305488151bada4f57fd
Jonathan Moore <jonm@duck.com>
2023-01-10 20:40:25 -0600

    Fix players not seeing disguises on join

--- FunnyPills/Items/DisguisePills.cs
+++ FunnyPills/Items/DisguisePills.cs
@@ -1,8 +1,10 @@
 ﻿using Exiled.API.Extensions;
+using Exiled.API.Features;
 using Exiled.API.Features.Attributes;
 using Exiled.API.Features.Spawn;
 using Exiled.CustomItems.API.Features;
 using Exiled.Events.EventArgs.Player;
+using MEC;
 using PlayerRoles;
 using System.Collections.Generic;
 
@@ -19,17 +21,47 @@ namespace FunnyPills.Items
         public override string Description { get; set; } = "Disguise yourself as an allied class";
         public override float Weight { get; set; } = 0;
         public override SpawnProperties SpawnProperties { get; set; }
+        private IDictionary<Player, RoleTypeId> playerDisguises { get; set; } = new Dictionary<Player, RoleTypeId>();
 
         protected override void SubscribeEvents()
         {
+            Exiled.Events.Handlers.Server.RoundStarted += OnRoundStart;
             Exiled.Events.Handlers.Player.UsedItem += OnUsedItem;
+            Exiled.Events.Handlers.Player.Verified += OnPlayerVerified;
             base.SubscribeEvents();
         }
 
         protected override void UnsubscribeEvents()
         {
+            Exiled.Events.Handlers.Server.RoundStarted -= OnRoundStart;
             Exiled.Events.Handlers.Player.UsedItem -= OnUsedItem;
+            Exiled.Events.Handlers.Player.Verified -= OnPlayerVerified;
+
+
             base.UnsubscribeEvents();
+        }
+
+        private void OnRoundStart()
+        {
+            playerDisguises.Clear();
+        }
+
+        // when new players join, send them the disguise packet
+        // so they see the fake appearance
+        private void OnPlayerVerified(VerifiedEventArgs ev)
+        {
+            // Wait a bit to let the non-fake packet go first
+            Timing.CallDelayed(1, () =>
+            {
+                foreach (KeyValuePair<Player, RoleTypeId> playerDisguise in playerDisguises)
+                {
+                    Player disguisedPlayer = playerDisguise.Key;
+                    RoleTypeId disguise = playerDisguise.Value;
+                    ev.Player.Connection.Send(
+                        new RoleSyncInfo(disguisedPlayer.ReferenceHub, disguise, ev.Player.ReferenceHub));
+
+                }
+            });
         }
 
         private void OnUsedItem(UsedItemEventArgs ev)
@@ -68,6 +100,7 @@ namespace FunnyPills.Items
                         }.RandomElement();
                         break;
                 }
+                playerDisguises.Add(ev.Player, disguise);
                 ev.Player.ChangeAppearance(disguise);
                 ev.Player.Broadcast(10, $"You now look like a <color={disguise.GetColor().ToHex()}>{disguise.GetFullName()}</color>!", Broadcast.BroadcastFlags.Normal, true);
             }
